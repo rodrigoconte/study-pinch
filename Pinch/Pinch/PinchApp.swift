70d3cacfc70cff2209660a97ee4873440e471aa3
Rodrigo Conte <rodrigo.conte-terceiro@cea.com.br>
2023-12-09 11:54:03 -0400

    Feat: Creting new project and settings

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Rodrigo Conte Oliveira on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
