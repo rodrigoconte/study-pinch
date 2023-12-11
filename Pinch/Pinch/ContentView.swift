14adf50a7051070e36cbf53a5060cd17269ba445
Rodrigo Conte <rodrigo.conte-terceiro@cea.com.br>
2023-12-11 10:06:51 -0400

    Feat: Including new actions into buttons on app - Created new group of buttons to change image scale by clicks

--- Pinch/Pinch/ContentView.swift
+++ Pinch/Pinch/ContentView.swift
@@ -82,6 +82,54 @@ struct ContentView: View {
                 alignment: .top
             )
             
+            // MARK: - CONTROLS
+            .overlay(
+                Group {
+                    HStack {
+                        // SCALE DOWN
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        // RESET
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        // SCALE UP
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                    }//- Hstack
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                }//- Group
+                    .padding(30),
+                alignment: .bottom
+            )
+            
         }//- Navigation view
         .navigationViewStyle(.stack)
     }//- Body
