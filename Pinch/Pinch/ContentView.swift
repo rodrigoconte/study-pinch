f59e6abd04f548c7dcf4c37a26357234c2c04791
Rodrigo Conte <rodrigo.conte-terceiro@cea.com.br>
2023-12-11 10:17:05 -0400

    feat: Including magnification gesture into app

--- Pinch/Pinch/ContentView.swift
+++ Pinch/Pinch/ContentView.swift
@@ -64,6 +64,27 @@ struct ContentView: View {
                                 }
                             })
                     )
+                
+                // MARK: - 3. MAGNIFICATION
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             }//- Zstack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
